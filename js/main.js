cee9e1b60aa5eb873ad63a94e8a7825e131ff22f
FFeyzi-web <feyzullayevferqn@gmail.com>
2026-05-15 11:57:17 +0400

    money

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-
-let i= [ 7 ,8 ,9,3,1,4, 'artyom' ,"hikmet"
-,'alma']
-
-i[9]=['salvar' , 'mayka']
+// let i= [ 7 ,8 ,9,3,1,4, 'artyom' ,"hikmet"
+// ,'alma',['salvar' , 'mayka']]
 
 
 
+// console.log(    i[9][1]    );
 
-console.log(    i[9]    );
+// let a = {
+//     'ad' : "artyom",
+//     'soyad' : "Feyzullayev",
+//     'Yash' : 20,
+// }
+
+// console.log(   a.Yash  );
+
+function ferqEtmez() {
+ console.log('artyom derslere hazirliqsiz gelir');
+}
+
