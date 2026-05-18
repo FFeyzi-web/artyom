335bc6a04d8c875c3b8294f644187307dcf5bd9d
FFeyzi-web <feyzullayevferqn@gmail.com>
2026-05-18 14:45:01 +0400

    end

--- js/main.js
+++ js/main.js
@@ -16,19 +16,20 @@
 function ferqEtmez() {
    
     
-    let eded1 = document.getElementById ('eded1').value;
-    let eded2 = document.getElementById ('eded2').value;
+    let eded1 = Number (document.getElementById ('eded1').value);
+    let eded2 = Number (document.getElementById ('eded2').value);
 
     let cem = eded1 + eded2;
 
-    console.log( cem );
+    document.getElementById('ekranaYazdir').innerText = cem;
+
     
 }
 
+ let duymeniBas = document.getElementById('deyerHesabla');
 
-// let duymeniBas = document.getElementById('deyerHesabla');
+duymeniBas.onclick = ferqEtmez
 
-// console.log(    duymeniBas.onclick('ferqEtmez')  );
 
 
 
